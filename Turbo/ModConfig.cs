b57b9fa9ec345ae5488a2af3e9987cc3537d7805
PrimmR <primmr@icloud.com>
2024-05-07 21:49:21 +0100

    Initial commit

---
+++ Turbo/ModConfig.cs
@@ -0,0 +1,18 @@
+﻿using StardewModdingAPI.Utilities;
+
+namespace Turbo
+{
+    /// <summary>Model for config.json.</summary>
+    public sealed class ModConfig
+    {
+        public int clockMode { get; set; } = 0; // 0 Regular, 1 Constant, 2 Frozen
+
+        public KeybindList decrementSpeedKeybind { get; set; } = KeybindList.Parse("OemComma");
+
+        public KeybindList incrementSpeedKeybind { get; set; } = KeybindList.Parse("OemPeriod");
+
+        public KeybindList resetSpeedKeybind { get; set; } = KeybindList.Parse("OemSemicolon");
+
+        public KeybindList cycleClockModeKeybind { get; set; } = KeybindList.Parse("OemQuotes");
+    }
+}
