93264820230b039fdca30bebcf82b4faf11b3cb0
PrimmR <primmr@icloud.com>
2025-04-20 11:41:37 +0100

    Implement compatibility with Mod Menu

--- Turbo/ModEntry.cs
+++ Turbo/ModEntry.cs
@@ -1,5 +1,7 @@
 ﻿using HarmonyLib;
 using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
 using StardewValley;
 
 namespace Turbo
@@ -32,6 +34,7 @@ namespace Turbo
         public override void Entry(IModHelper helper)
         {
             LoadConfig();
+            helper.Events.GameLoop.GameLaunched += OnGameLaunched;
 
             // Harmony patches
             var harmony = new Harmony(this.ModManifest.UniqueID);
@@ -55,6 +58,83 @@ namespace Turbo
             HUDPatcher.Initialise(this.Monitor);
         }
 
+        /// <summary>Called on game launch.</summary>
+        /// Used for GenericModConfigMenu
+        private void OnGameLaunched(object sender, GameLaunchedEventArgs e)
+        {
+
+            // get Generic Mod Config Menu's API (if it's installed)
+            var configMenu = this.Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+            if (configMenu is null)
+                return;
+
+            // register mod
+            configMenu.Register(
+                mod: this.ModManifest,
+                reset: () => Config = new ModConfig(),
+                save: () => this.Helper.WriteConfig(Config)
+            );
+
+            // add some config options
+            string[] clock_modes = {
+                Helper.Translation.Get("ClockMode.Regular"),
+                Helper.Translation.Get("ClockMode.Constant"),
+                Helper.Translation.Get("ClockMode.Frozen")
+            };
+
+            configMenu.AddSectionTitle(
+                    mod: this.ModManifest,
+                    text: () => "Options"
+                );
+
+            configMenu.AddTextOption(
+                mod: this.ModManifest,
+                name: () => Helper.Translation.Get("ClockMode.ConfigName"),
+                tooltip: () => Helper.Translation.Get("ClockMode.ConfigTooltip"),
+                getValue: () => clock_modes[Config.clockMode],
+                setValue: value => Config.clockMode = Array.IndexOf(clock_modes, value),
+                allowedValues: clock_modes
+            );
+
+
+            configMenu.AddSectionTitle(
+                    mod: this.ModManifest,
+                    text: () => "Keybinds"
+                );
+
+            configMenu.AddKeybindList(
+                mod: this.ModManifest,
+                name: () => Helper.Translation.Get("DecrementSpeed.ConfigName"),
+                tooltip: () => Helper.Translation.Get("DecrementSpeed.ConfigTooltip"),
+                getValue: () => Config.decrementSpeedKeybind,
+                setValue: value => Config.decrementSpeedKeybind = value
+            );
+
+            configMenu.AddKeybindList(
+                mod: this.ModManifest,
+                name: () => Helper.Translation.Get("IncrementSpeed.ConfigName"),
+                tooltip: () => Helper.Translation.Get("IncrementSpeed.ConfigTooltip"),
+                getValue: () => Config.incrementSpeedKeybind,
+                setValue: value => Config.incrementSpeedKeybind = value
+            );
+
+            configMenu.AddKeybindList(
+                mod: this.ModManifest,
+                name: () => Helper.Translation.Get("ResetSpeed.ConfigName"),
+                tooltip: () => Helper.Translation.Get("ResetSpeed.ConfigTooltip"),
+                getValue: () => Config.resetSpeedKeybind,
+                setValue: value => Config.resetSpeedKeybind = value
+            );
+
+            configMenu.AddKeybindList(
+                 mod: this.ModManifest,
+                 name: () => Helper.Translation.Get("CycleClockMode.ConfigName"),
+                 tooltip: () => Helper.Translation.Get("CycleClockMode.ConfigTooltip"),
+                 getValue: () => Config.cycleClockModeKeybind,
+                 setValue: value => Config.cycleClockModeKeybind = value
+             );
+        }
+
         /// <summary>Resets Turbo's internal timer to 0.</summary>
         public static void ResetClock()
         {
@@ -69,7 +149,7 @@ namespace Turbo
             if (Config.clockMode < 0 || Config.clockMode > 2)
             {
                 Config.clockMode = 0;
-                Helper.WriteConfig(ModEntry.Config);
+                Helper.WriteConfig(Config);
                 Monitor.Log("Clock mode set to invalid state - Initialised to 0 (regular)", LogLevel.Error);
             }
         }
